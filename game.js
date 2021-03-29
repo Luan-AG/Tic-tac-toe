f0fea9e5efcbe48ffe812fc009f87063f0b06f57
Luan Goncalves <luanarcharde@gmail.com>
2021-03-29 14:15:14 +0200

    Remove unnecessary comments

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-const cellElements = document.querySelectorAll('[data-cell]'); //Array of cells
+const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
 const winningMessageElement = document.getElementById('winningMessage');
 const restartButton = document.getElementById('restartButton');
@@ -15,7 +15,7 @@ const winCombinations = [
     [0, 4, 8],
     [2, 4, 6]
 ];
-let circleTurn; //variable to check whose turn it is
+let circleTurn;
 
 startGame();
 
@@ -27,28 +27,28 @@ function startGame() {
         cell.classList.remove(xClass);
         cell.classList.remove(oClass);
         cell.removeEventListener('click', handleClick);
-        cell.addEventListener('click', handleClick, {once: true}); //Listen to a click on each cell just once and then run the function
+        cell.addEventListener('click', handleClick, {once: true});
     })
     setBoardHoverClass();
     winningMessageElement.classList.remove('show');
 }
 
-function handleClick(e) { //When clicked on a cell
+function handleClick(e) { 
     const cell = e.target;
-    const currentClass = circleTurn ? oClass : xClass //if 'circleTurn' = true, then set the variable to 'oClass', else set it to xClass
-    cell.classList.add(currentClass); //populate the UI. Get the cell clicked and add 'currentClass' to the element targeted
+    const currentClass = circleTurn ? oClass : xClass 
+    cell.classList.add(currentClass); 
     
-    if (checkWin(currentClass)) { //if checkWin() returns true, run inside (true means the player wins)
-        endGame(false); //show the msg 'wins'
-    } else if (isDraw()) { //CheckWin() returns false, check if isDraw() is true then run inside
-        endGame(true); //show the msg 'draw'
-    } else { // isDraw() is false then run inside (game is not end)
-        circleTurn = !circleTurn; //swap players turn
-        setBoardHoverClass(); //give the hover efect
+    if (checkWin(currentClass)) { 
+        endGame(false);
+    } else if (isDraw()) {
+        endGame(true); 
+    } else { 
+        circleTurn = !circleTurn; 
+        setBoardHoverClass();
     }
 }
 
-function endGame(draw) { //Message display function
+function endGame(draw) { 
     if (draw) {
         winningMessageTextElement.innerText = "It's a Draw!";
     } else {
@@ -58,8 +58,8 @@ function endGame(draw) { //Message display function
 }
 
 function isDraw() {
-    return [...cellElements].every(cell => { //Get the elements inside the array cellElements and loop through it. If all the array elements pass the test it returns true. PS: We have to destructure the 'cellElement' because it doesn't have all the methods available, so we use [... cellElements] syntax to get around it.
-        return cell.classList.contains(xClass) || cell.classList.contains(oClass);// Test: Return true if each cell has a class of 'xClass' or 'oClass'
+    return [...cellElements].every(cell => { 
+        return cell.classList.contains(xClass) || cell.classList.contains(oClass);
     })
 }
 
@@ -74,9 +74,9 @@ function setBoardHoverClass() {
 }
 
 function checkWin(currentClass) {
-    return winCombinations.some(combination => { // Get the array with the winning combinations and loop through it. If some/any of the array elements pass the test it returns true.
-        return combination.every(index => { // Test: Get the elements inside the array (eg: [0, 1 ,2]) and loop through it. If all the array elements pass the test it returns true.
-            return cellElements[index].classList.contains(currentClass);// Test :Return true if each position on the board that is equivalent to values in the array have a class of the currentClass. Else returns false.
+    return winCombinations.some(combination => { 
+        return combination.every(index => { 
+            return cellElements[index].classList.contains(currentClass);
         })
     })
 }
